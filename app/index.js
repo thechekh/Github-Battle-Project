9b093247c219faa9006017df79f3870be263b10a
DanilChekhRez <ymovchandev@gmail.com>
2019-04-23 10:27:46 +0300

    add start script

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-class index extends React.Component {
+require("./index.css");
+class App extends React.Component {
     render() {
         return (
             <div>
--- webpack.config.js
+++ webpack.config.js
@@ -21,3 +21,4 @@ module.exports = {
   ],
   mode: "development"
 }
+
